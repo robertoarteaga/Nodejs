aaa812df0d8bfdb0b780c77335d9674231430dc2
Daniel Olvera <danielolvera3f@gmail.com>
2019-05-29 18:01:33 -0500

    App

---
+++ Proyecto/app.js
@@ -0,0 +1,26 @@
+const morgan = require("morgan");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('./middleware/cors');
+const app = express();
+
+const camiones = require('./routes/camiones');
+const usuarios = require('./routes/usuarios');
+const conductores = require('./routes/conductores');
+const terminal = require('./routes/terminal');
+
+
+
+//MIDDLEWARE
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors);
+
+app.use('/camiones',camiones);
+app.use('/usuarios',usuarios);
+app.use('/conductores',conductores);
+app.use('/terminal',terminal);
+
+
+
